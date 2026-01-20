944a68d70393ae3f7a68dbb922200e36e2f73eb8
itsmeishwar <ishwarawasthi3@gmail.com>
2026-01-20 13:54:44 +0545

    Make the Password reset page Page

---
+++ src/components/PasswordReset.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PasswordReset: React.FC = () => {
+  return (
+    <div>
+      <h1>Password Reset</h1>
+      <p>This is the Password Reset page.</p>
+    </div>
+  );
+};
+
+export default PasswordReset;

997d62ca493edc6636e550e0f169baaac5482d69
itsmeishwar <ishwarawasthi3@gmail.com>
2026-01-21 13:10:41 +0545

    make the stat card  page

---
+++ src/components/StatCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StatCard: React.FC = () => {
+  return (
+    <div>
+      <h1>Stat Card</h1>
+      <p>This is the Stat Card component.</p>
+    </div>
+  );
+};
+
+export default StatCard;
